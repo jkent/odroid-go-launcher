a536102888f73693c0eb28200db422865b2b0f25
Jeff Kent <jeff@jkent.net>
2018-07-26 10:58:36 -0500

    working app demo

--- main/main.c
+++ main/main.c
@@ -1,19 +1,23 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_ota_ops.h"
+#include "esp_partition.h"
+#include "esp_spi_flash.h"
 #include "esp_system.h"
 #include "driver/gpio.h"
 
 #include "../components/hardware/display.h"
 #include "../components/hardware/backlight.h"
 #include "../components/hardware/keypad.h"
+#include "../components/hardware/sdcard.h"
 
 #include "gbuf.h"
 #include "tf.h"
 #include "OpenSans_Regular_11X12.h"
 
-#include "image_cone.h"
-
+#include <errno.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 
 static volatile uint16_t keypad_debounced = 0;
@@ -33,6 +37,10 @@ void app_main(void)
 {
     struct gbuf *fb = display_init();
     memset(fb->pixel_data, 0, fb->width * fb->height * fb->bytes_per_pixel);
+
+    esp_err_t err = sdcard_init("/sd");
+    if (err) abort();
+
     backlight_init();
     keypad_init();
 
@@ -55,6 +63,39 @@ void app_main(void)
     strcpy(s, "Loading...");
     m = tf_get_str_metrics(tf, s);
     tf_draw_str(fb, tf, s, DISPLAY_WIDTH/2 - m.width/2, DISPLAY_HEIGHT/2 - m.height/2);
-
     display_draw();
+
+    FILE *f = fopen("/sd/apps/hello-world.bin", "rb");
+    if (!f) {
+        printf("errno: %d\n", errno);
+        abort();
+    }
+
+    fseek(f, 0, SEEK_END);
+    int size = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    esp_ota_handle_t ota_handle;
+
+    const esp_partition_t *part = esp_partition_find_first(ESP_PARTITION_TYPE_APP, ESP_PARTITION_SUBTYPE_APP_OTA_1, NULL);
+    err = esp_ota_begin(part, size, &ota_handle);
+    if (err) abort();
+
+    char *buf = malloc(4096);
+    if (!buf) abort();
+
+    while (true) {
+        size_t len = fread(buf, 1, 4096, f);
+        if (len == 0) break;
+
+        err = esp_ota_write(ota_handle, buf, len);
+        if (err) abort();
+    }
+
+    err = esp_ota_end(ota_handle);
+    if (err) abort();
+
+    esp_ota_set_boot_partition(part);
+
+    esp_restart();
 }
